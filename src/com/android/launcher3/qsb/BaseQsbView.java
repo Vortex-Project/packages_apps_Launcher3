AOSiP/platform_packages_apps_Launcher3

Launcher3: Set search type based on Gsa
Update the search type in the app drawer based on the Google app availability
Change-Id: Ib3ad11eea03ba43987c81d43cca2e7f85740a501

src/com/android/launcher3/qsb/BaseQsbView.java
@@ -50,6 +50,8 @@ import android.widget.TextView;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherCallbacks;
 
+import com.android.internal.util.aosip.aosipUtils;
+
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Insettable;
 import com.android.launcher3.Launcher;
@@ -174,9 +176,10 @@ public abstract class BaseQsbView extends FrameLayout implements OnClickListener
     }
 
     public void loadMicViews() {
+        boolean hasGsa = aosipUtils.isPackageInstalled(getContext(), LauncherCallbacks.SEARCH_PACKAGE);
         mMicIconView = (ImageView) findViewById(R.id.mic_icon);
-        mMicIconView.setOnClickListener(this);
-        mMicIconView.setVisibility(View.VISIBLE);
+        mMicIconView.setOnClickListener(hasGsa ? this : null);
+        mMicIconView.setVisibility(hasGsa ? View.VISIBLE : View.GONE);
         setTouchDelegate(mQsbDelegate);
         requestLayout();
     }

Vortex-Project/packages_apps_Launcher3

Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

src/com/android/launcher3/qsb/BaseQsbView.java
@@ -50,6 +50,8 @@ import android.widget.TextView;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherCallbacks;
 
+import com.android.internal.util.aosmp.aosmpUtils;
+
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Insettable;
 import com.android.launcher3.Launcher;
@@ -174,9 +176,10 @@ public abstract class BaseQsbView extends FrameLayout implements OnClickListener
     }
 
     public void loadMicViews() {
+        boolean hasGsa = aosmpUtils.isPackageInstalled(getContext(), LauncherCallbacks.SEARCH_PACKAGE);
         mMicIconView = (ImageView) findViewById(R.id.mic_icon);
-        mMicIconView.setOnClickListener(this);
-        mMicIconView.setVisibility(View.VISIBLE);
+        mMicIconView.setOnClickListener(hasGsa ? this : null);
+        mMicIconView.setVisibility(hasGsa ? View.VISIBLE : View.GONE);
         setTouchDelegate(mQsbDelegate);
         requestLayout();
     }