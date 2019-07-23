AOSiP/platform_packages_apps_Launcher3

Notification gesture: use our aosipUtils
Change-Id: Iac027065351419f76008ed23e406362d6bae20c0
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

src/com/android/launcher3/Workspace.java
@@ -310,7 +310,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
                     if (e2.getY() - e1.getY() > 160/*min distance*/
                             && Math.abs(velocityY) > 250/*min speed*/) {
                         if(Utilities.useNotificationsGesture(context)) {
-                            openNotifications();
+                            ActionUtils.toggleNotifications();
                         }
                     }
                 } catch (Exception e) {
@@ -341,17 +341,6 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
     public void setGestures(int mode) {
         mGestureMode = mode;
-    }
-
-    private boolean openNotifications() {
-        try {
-            Class.forName("android.app.StatusBarManager")
-                    .getMethod("expandNotificationsPanel")
-                    .invoke(mLauncher.getSystemService("statusbar"));
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
     }
 
     public boolean checkCustomGestures(MotionEvent ev) {

Vortex-Project/packages_apps_Launcher3

Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

src/com/android/launcher3/Workspace.java
@@ -310,7 +310,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
                     if (e2.getY() - e1.getY() > 160/*min distance*/
                             && Math.abs(velocityY) > 250/*min speed*/) {
                         if(Utilities.useNotificationsGesture(context)) {
-                            openNotifications();
+                            aosmpUtils.toggleNotifications();
                         }
                     }
                 } catch (Exception e) {
@@ -341,17 +341,6 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
     public void setGestures(int mode) {
         mGestureMode = mode;
-    }
-
-    private boolean openNotifications() {
-        try {
-            Class.forName("android.app.StatusBarManager")
-                    .getMethod("expandNotificationsPanel")
-                    .invoke(mLauncher.getSystemService("statusbar"));
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
     }
 
     public boolean checkCustomGestures(MotionEvent ev) {